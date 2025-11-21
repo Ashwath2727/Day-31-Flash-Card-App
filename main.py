e883424e0fda827ba262770bdf6737488b6d9583
Ashwath S <miniash123@outlook.com>
2025-11-21 19:14:46 +0530

    added logic for creating flash cards and flipping them

--- main.py
+++ main.py
@@ -1,29 +1,63 @@
+import random
 import tkinter as tk
-from tkinter import PhotoImage
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ------------------- Taking data from csv ---------------
+french_words_data = pd.read_csv("data/french_words.csv")
+french_words_dict = french_words_data.to_dict(orient="records")
+print(french_words_dict)
+
+current_card = {}
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+
+    current_card = random.choice(french_words_dict)
+    print(current_card)
+
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+
+    canvas.itemconfig(card_background, image=card_front_image)
+
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
+
+# ---------------------------- UI Build -------------------------------
 window = tk.Tk()
 window.title("Flashy")
 window.configure(background=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = tk.Canvas(width=800, height=526)
 card_front_image = tk.PhotoImage(file="images/card_front.png")
-canvas.create_image(410, 270, image=card_front_image)
+card_back_image = tk.PhotoImage(file="images/card_back.png")
+
+card_background = canvas.create_image(410, 270, image=card_front_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 right_image = tk.PhotoImage(file="images/right.png")
-right_button = tk.Button(image=right_image, highlightthickness=0)
-right_button.grid(row=1, column=0, pady=20)
+right_button = tk.Button(image=right_image, highlightthickness=0, command=next_card)
+right_button.grid(row=1, column=1, pady=20)
 
 wrong_image = tk.PhotoImage(file="images/wrong.png")
-wrong_button = tk.Button(image=wrong_image, highlightthickness=0)
-wrong_button.grid(row=1, column=1, pady=20)
+wrong_button = tk.Button(image=wrong_image, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0, pady=20)
 
-
+next_card()
 
 window.mainloop()
 
